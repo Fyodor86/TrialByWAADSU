af8129148bbdee69c0e60d0894e2c0920e78b918
Fedor Baryshnikov <fyodor.baryshnikov@gmail.com>
2021-11-04 07:52:34 +0300

    Initial Commit

---
+++ TrialByWAADSU/TrialByWAADSUApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrialByWAADSUApp.swift
+//  TrialByWAADSU
+//
+//  Created by Fedor Baryshnikov on 04.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrialByWAADSUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
